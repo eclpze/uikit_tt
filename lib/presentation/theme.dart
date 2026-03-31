b7d4275018e84272903352f53b6dce5eee426a85
Камилла Губайдуллина <gubai.k60@gmail.com>
2026-03-31 22:33:45 +0300

    Создание класса для тектстовых стилей

--- lib/presentation/theme.dart
+++ lib/presentation/theme.dart
@@ -12,3 +12,9 @@ abstract class AppColors {
   static const Color dark_pink = Color(0xffF22E63);
 }
 
+// Класс для текстовых стилей, Дата создания: 31.03.2026, Автор создвния: 5
+abstract class AppTypography {
+  static TextStyle title1Semibold = TextStyle(fontFamily: 'PublicSans', fontSize: 24.ts, fontWeight: FontWeight.w600, height: 28.fh, letterSpacing: 24.ts * 0.0033);
+  static TextStyle title2Regular = TextStyle(fontFamily: 'PublicSans', fontSize: 20.ts, fontWeight: FontWeight.w400, height: 28.fh, letterSpacing: 20.ts * 0.0038);
+  static TextStyle title2Semibold = TextStyle(fontFamily: 'PublicSans', fontSize: 17.ts, fontWeight: FontWeight.w400, height: 24.fh, letterSpacing: 0);
+}
